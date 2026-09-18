13e325bb81d3a62b8962f5f55c555b0b1bb074ad
daslan <daslan@fiu.edu>
2026-09-18 16:53:02 -0400

    switched to using the client in each of the functions vs top level, will look into sessions later.  This was causing issues with going thru regions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import boto3
 import argparse
+from time import sleep
 from pprint import pprint as pp
 
 parser = argparse.ArgumentParser("AWS ToolBox")
@@ -18,7 +19,8 @@ s3 = boto3.client('s3')
 ec2 = boto3.client('ec2')
 
 ######################################### Regions Functions #####################################
-def list_all_regions(client):
+def list_all_regions():
+    client = boto3.client('ec2')
     response_reg = client.describe_regions()
     regions = [] 
     response_az = client.describe_availability_zones(
@@ -36,8 +38,9 @@ def list_all_regions(client):
 
 
 ########################################### S3 Functions #######################################
-def list_all_s3_buckets(client):
+def list_all_s3_buckets():
     print("Working")
+    client = boto3.client('s3')
     response = client.list_buckets()
     bucket_names = []
 
@@ -47,7 +50,8 @@ def list_all_s3_buckets(client):
     return bucket_names
 
 
-def s3_storage_size(client, bucket_names):
+def s3_storage_size(bucket_names):
+    client = boto3.client('s3')
     bucket_and_size = {}
 
     for name in bucket_names:
@@ -66,32 +70,41 @@ def s3_storage_size(client, bucket_names):
 
 
 ######################################## EC2 Functions #######################################
-def list_all_ec2(client,regions):
+def list_all_ec2(regions):
+    regional_running_instances = {}
+    running_instances = []
 
     for region in regions:
-        boto3.setup_default_session(region_name=region)
+        client = boto3.client('ec2', region_name=region)
         paginator = client.get_paginator('describe_instances')
     
         for page in paginator.paginate(Filters=[
-            #{ 
-            #  'Name': 'instance-state-name',
-            #  'Values': ['running'],
-            #},
+            { 
+              'Name': 'instance-state-name',
+              'Values': ['running'],
+            },
             ],
             ):
-            pp(page)
-           #pp(type(page.get('Reservations')))
+            for reservation in page.get('Reservations', []):
+                for instance in reservation.get('Instances', []):
+                    instance_id = instance.get('InstanceId')
+                    running_instances.append = instance_id
+                    pp(instance_id)
+                    pp('-'*50)
+        regional_running_instances['region'] = running_instances
+
+    return regional_running_instances
 
   
 ######################################## EBS Functions #######################################
-def list_all_ebs_volumes(client,regions):
+def list_all_ebs_volumes(regions):
     regional_attached_volumes = {}
     regional_detached_volumes = {}
 
     for region in regions:
+        client = boto3.client('ec2', region_name=region)
         attached_volumes = {}
         detached_volumes = {}
-        boto3.setup_default_session(region_name=region)
         paginator = client.get_paginator('describe_volumes')
     
         for page in paginator.paginate(Filters=[{
@@ -127,17 +140,17 @@ def list_all_ebs_volumes(client,regions):
 
 
 def main():
-    ec2_regions = list_all_regions(ec2)
+    ec2_regions = list_all_regions()
 
     if args.list_s3_buckets:
-        names = list_all_s3_buckets(s3)
-        pp(s3_storage_size(s3, names))
+        names = list_all_s3_buckets()
+        pp(s3_storage_size(names))
 
     if args.list_all_ec2:
-        pp(list_all_ec2(ec2,ec2_regions))
+        list_all_ec2(ec2_regions)
 
     if args.list_all_ebs:
-        pp(list_all_ebs_volumes(ec2,ec2_regions))
+        pp(list_all_ebs_volumes(ec2_regions))
 
 
 if __name__ == "__main__":
